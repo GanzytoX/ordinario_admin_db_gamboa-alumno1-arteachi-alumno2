8db44c1138faa53315e52d6c8badcfd89db97ee2
Gonzalo Gamboa <gonzalo.gb11.ggb@gmail.com>
2024-12-08 15:00:49 -0600

    Update app.js

--- app.js
+++ app.js
@@ -4,14 +4,13 @@ require('dotenv').config();
 const app = express();
 app.use(bodyParser.json());
 
-// Importar rutas
 const estudiantesRoutes = require('./routes/estudiantes')
 
 // Rutas
 app.use('/api/estudiantes', estudiantesRoutes)
 
 // Puerto e inicializacion del servidor
-const PORT = 3011;
+const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
